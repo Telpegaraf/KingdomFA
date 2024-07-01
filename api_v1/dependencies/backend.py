8005749bc0caf81519b5d0630bec2df5b9e3b0f4
Telpegaraf <telpegaraf@gmail.com>
2024-07-01 23:51:41 +0400

    added authentication backend

---
+++ api_v1/dependencies/backend.py
@@ -0,0 +1,13 @@
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+)
+
+from api_v1.authentication.transport import bearer_transport
+from api_v1.dependencies.strategy import get_database_strategy
+
+
+authentication_backend = AuthenticationBackend(
+    name='access-tokens-db',
+    transport=bearer_transport,
+    get_strategy=get_database_strategy
+)
