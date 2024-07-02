ea61f1decd3805fd44fe2e3bb4f472926c15d1dd
Telpegaraf <telpegaraf@gmail.com>
2024-07-02 12:45:14 +0400

    added user chemas

--- api_v1/authentication/user_manager.py
+++ api_v1/authentication/user_manager.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Optional, TYPE_CHECKING
-from fastapi_users import BaseUserManager
+from fastapi_users import BaseUserManager, IntegerIDMixin
 
 from core.config import settings
 from api_v1.models.user_test import TestUser
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-class UserManager(Base, BaseUserManager[TestUser, UserIdType]):
+class UserManager(IntegerIDMixin, BaseUserManager[TestUser, UserIdType]):
     reset_password_token_secret = settings.jwt_database_settings.get("reset_password_secret")
     verification_token_secret = settings.jwt_database_settings.get("verification_password_secret")
 
--- api_v1/dependencies/user_manager.py
+++ api_v1/dependencies/user_manager.py
@@ -1,3 +1,15 @@
+from fastapi import Depends
+from typing import Annotated, TYPE_CHECKING
+from api_v1.authentication.user_manager import UserManager
+from api_v1.dependencies.users import get_users_db
+if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 
-async def get_user_manager
+async def get_user_manager(
+        users_db: Annotated[
+            SQLAlchemyUserDatabase,
+            Depends(get_users_db)
+        ],
+):
+    yield UserManager(users_db)
