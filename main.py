ebde2cbe595364be3ec3f3b6d70adde02ea737cc
Telpegaraf <telpegaraf@gmail.com>
2024-08-06 16:44:36 +0400

    added logging

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routers import include_routers
+import logging
 
 from action.create_super_user import create_superuser
 
 app = FastAPI()
 include_routers(app)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='kingdom.log',
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
